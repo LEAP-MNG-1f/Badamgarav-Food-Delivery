b996f0d53e649d8dec8af68881272e8ef7522250
Бадамгарав Орги <24LP1595@G401-CC0119.local>
2024-11-13 13:08:10 +0800

    BE

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 8000;
 
+server.use(cors());
 server.get("/", (request, response) => {
   response.send("Hello world");
 });
