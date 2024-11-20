49b9ec3e0885aef4264f91b363fadb882b5882f3
Бадамгарав Орги <24LP1595@G401-CC0119.local>
2024-11-20 12:25:27 +0800

    FastDelivery hiv

--- front-end/src/app/_components/homepage/Meals.tsx
+++ front-end/src/app/_components/homepage/Meals.tsx
@@ -1,36 +1,148 @@
-import * as React from "react";
+// import * as React from "react";
+"use client";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import CardActionArea from "@mui/material/CardActionArea";
 import { Stack } from "@mui/material";
+import { CardList } from "../lesson/CardList";
 
-export default function ActionMealCard() {
-  return (
-    <Stack
-      className="!w-full !flex !justify-center mt-[120px] mb-[120px]"
-      direction="row"
-    >
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" sx={{ color: "text.secondary" }}>
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Stack>
-  );
+const foodMockData: FoodType[] = [
+  {
+    _id: "1",
+    name: "Өглөөний хоол",
+    price: 14800,
+    image: "./1Egg.png",
+    ingredient: "Beef Patty, Cheese, Lettuce, Tomato, Bun",
+  },
+  {
+    _id: "2",
+    name: "Зайрмаг",
+    price: 4800,
+    image: "./2IceCream.png",
+    ingredient: "Tomato Sauce, Mozzarella, Basil, Olive Oil",
+  },
+  {
+    _id: "3",
+    name: "Өглөөний хоол",
+    price: 24800,
+    image: "./3Fruits.png",
+    ingredient: "Chicken, Romaine Lettuce, Croutons, Parmesan, Caesar Dressing",
+  },
+  {
+    _id: "4",
+    name: "Breakfast",
+    price: 24800,
+    image: "./4Vandui.png",
+    ingredient: "Spaghetti, Ground Beef, Tomato Sauce, Onion, Garlic",
+  },
+  {
+    _id: "5",
+    name: "Main Pizza ",
+    price: 34800,
+    image: "./5Pizza.png",
+    ingredient: "Broccoli, Carrots, Bell Peppers, Soy Sauce, Ginger",
+  },
+  {
+    _id: "6",
+    name: "Food tart",
+    price: 12900,
+    image: "./6Tart.png",
+    ingredient: "Salmon Fillet, Lemon, Olive Oil, Garlic, Parsley",
+  },
+  {
+    _id: "7",
+    name: "Өглөөний хоол",
+    price: 14499,
+    image: "./7Morning.png",
+    ingredient: "Ground Beef, Taco Shells, Lettuce, Cheese, Salsa",
+  },
+  {
+    _id: "8",
+    name: "Зутан шөл",
+    price: 54549,
+    image: "./8Soup.png",
+    ingredient: "Avocado, Toast, Lemon Juice, Chili Flakes",
+  },
+  {
+    _id: "9",
+    name: "Чихэрлэг тахиа",
+    price: 34599,
+    image: "./9Chicken.png",
+    ingredient: "Cocoa, Sugar, Butter, Flour, Eggs",
+  },
+  {
+    _id: "10",
+    name: "Lunch",
+    price: 24800,
+    image: "./10Lunch.png",
+    ingredient: "Mango, Yogurt, Milk, Honey",
+  },
+  {
+    _id: "11",
+    name: "Apple pie",
+    price: 20000,
+    image: "./11Sandwich.png",
+    ingredient: "Potatoes, Salt, Oil",
+  },
+  {
+    _id: "12",
+    name: "Сэндвич",
+    price: 24500,
+    image: "./12ApplePie.png",
+    ingredient: "Chicken Wings, BBQ Sauce, Garlic, Paprika",
+  },
+  {
+    _id: "13",
+    name: "Торт",
+    price: 54800,
+    image: "./13Cake.png",
+    ingredient: "Flour, Eggs, Milk, Butter, Maple Syrup",
+  },
+  {
+    _id: "14",
+    name: "Oreo shake",
+    price: 15000,
+    image: "./14Shake.png",
+    ingredient: "Cucumber, Tomato, Feta Cheese, Olives, Olive Oil",
+  },
+  {
+    _id: "15",
+    name: "Chocolate",
+    price: 14900,
+    image: "./15Choco.png",
+    ingredient: "Chicken, Butter, Tomato, Cream, Spices",
+  },
+  {
+    _id: "16",
+    name: "Yoghurt",
+    price: 50000,
+    image: "./16Yougurt.png",
+    ingredient: "Veggie Patty, Lettuce, Tomato, Cheese, Bun",
+  },
+];
+
+export default function ActionMealCard(props: CardListProps) {
+  return <CardList foods={foodMockData} />;
 }
+
+// <Card sx={{ maxWidth: 345 }}>
+// <CardActionArea>
+//   <CardMedia
+//     component="img"
+//     height="140"
+//     image="/static/images/cards/contemplative-reptile.jpg"
+//     alt="green iguana"
+//   />
+//   <CardContent>
+//     <Typography gutterBottom variant="h5" component="div">
+//       Lizard
+//     </Typography>
+//     <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//       Lizards are a widespread group of squamate reptiles, with over
+//       6,000 species, ranging across all continents except Antarctica
+//     </Typography>
+//   </CardContent>
+// </CardActionArea>
+// </Card>
